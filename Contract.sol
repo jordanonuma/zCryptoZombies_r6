f502147f40054a35df5b1ecbec1d3bf2e0d9223f
jordanonuma <jordanonuma@outlook.com>
2020-02-19 06:16:10 -1000

    Added 'emit' to start communications with 'NewZombie' event.

--- Contract.sol
+++ Contract.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
     event NewZombie(uint zombieId, string name, uint, dna);
-    
+
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
@@ -15,6 +15,7 @@ contract ZombieFactory {
 
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
+        emit NewZombie(zombieId, _name, dna);
     } //end createZombie()
 
     function _generateRandomDna(string memory _str) private view returns(uint) {
