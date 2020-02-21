69fbb9d4b43df5700b4facb38babab96e8d7bbf9
jordanonuma <jordanonuma@outlook.com>
2020-02-21 06:12:55 -1000

    Incremented user's zombie count.

--- Contract.sol
+++ Contract.sol
@@ -19,6 +19,7 @@ contract ZombieFactory {
         zombies.push(Zombie(_name, _dna));
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
         zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     } //end createZombie()
 
