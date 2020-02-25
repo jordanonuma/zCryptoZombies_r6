0328a651a21f2f474c943c211512c90c43da7c27
jordanonuma <jordanonuma@outlook.com>
2020-02-25 06:06:24 -1000

    Required that user owns zombie.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -3,6 +3,6 @@ import "./ZombieFactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply (uint _zombieId, uint _targetDna) public {
-      
+      require(msg.sender == zombieToOwner[_zombieId]);
     } //end function feedAndMultiply()
 } //end ZombieFeeding{}
