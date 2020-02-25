6dc502df9219e3ce689f23cef42f2c94439a0d50
jordanonuma <jordanonuma@outlook.com>
2020-02-25 06:04:03 -1000

    Added fraame of function that will be called for zombie attacks.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -2,5 +2,7 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./ZombieFactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
-    
+    function feedAndMultiply (uint _zombieId, uint _targetDna) public {
+      
+    } //end function feedAndMultiply()
 } //end ZombieFeeding{}
