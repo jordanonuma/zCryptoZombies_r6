97e978b7f56747602f71d458403bd3dfa443ff36
jordanonuma <jordanonuma@outlook.com>
2020-03-01 09:04:06 -1000

    Indented for consistent formatting.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -21,15 +21,15 @@ contract ZombieFeeding is ZombieFactory {
     KittyInterface kittyContract = KittyInterface(ckAddress);
 
     function feedAndMultiply (uint _zombieId, uint _targetDna) public {
-      require(msg.sender == zombieToOwner[_zombieId]);
-      Zombie storage myZombie = zombies[_zombieId];
+        require(msg.sender == zombieToOwner[_zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
 
-      uint _targetDna = _targetDna % dnaModulus;
-      newDna = (myZombie.dna +_targetDna)/2;
-      _createZombie("NoName", newDna);
+        uint _targetDna = _targetDna % dnaModulus;
+        newDna = (myZombie.dna +_targetDna)/2;
+        _createZombie("NoName", newDna);
     } //end function feedAndMultiply()
 
     function feedOnKitty(uint _zombieId, uint _kittyId) public {
-      uint kittyDna;
+        uint kittyDna;
     } //end function feedOnKitty()
 } //end ZombieFeeding{}
