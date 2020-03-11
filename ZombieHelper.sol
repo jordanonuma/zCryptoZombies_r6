eb3aa3b06e56efb0b70dd1d95b9fcdce9988e944
jordanonuma <jordanonuma@outlook.com>
2020-03-11 05:30:44 -1000

    Revised for syntax.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -7,6 +7,6 @@ contract ZombieHelper is ZombieFeeding {
     modifier aboveLevel(uint _level, uint _zombieId) {
         require(zombies[_zombieId].level >= _level);
         _;
-    } /end modifier aboveLevel()
+    } //end modifier aboveLevel()
 
 } //end ZombieHelper{}
