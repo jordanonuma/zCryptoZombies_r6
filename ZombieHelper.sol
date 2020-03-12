1b8bbdc5843b33c5da4749a6ab12b0806dd6761e
jordanonuma <jordanonuma@outlook.com>
2020-03-12 05:56:52 -1000

    Added frame of function that is callable within the same contract without using gas (i.e. a 'view' function).

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -19,4 +19,8 @@ contract ZombieHelper is ZombieFeeding {
         zombies[_zombieId].dna = _newDna;
     } //end function changeDna()
 
+    function getZombiesByOwner(address _owner) external view {
+    
+    } //end function getZombiesByOwner()
+
 } //end ZombieHelper{}
