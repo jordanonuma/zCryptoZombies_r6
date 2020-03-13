a6bf2abd25646f0aa71f1782cede99263b2b46e2
jordanonuma <jordanonuma@outlook.com>
2020-03-13 06:09:01 -1000

    Returns the uint[] array 'result'.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -21,6 +21,7 @@ contract ZombieHelper is ZombieFeeding {
 
     function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
         uint[] memory result = new uint[](ownerZombieCount[_owner]);
+        return result;
     } //end function getZombiesByOwner()
 
 } //end ZombieHelper{}
