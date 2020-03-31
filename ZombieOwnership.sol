9a449ad9ca9399a99b6eba1756877926cecfb630
jordanonuma <jordanonuma@outlook.com>
2020-03-31 06:26:31 -1000

    Defined mapping of each zombie to user with approvals to send it.

--- ZombieOwnership.sol
+++ ZombieOwnership.sol
@@ -3,6 +3,7 @@ import "./ZombieAttack.sol";
 import "./ERC721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
+  mapping zombieApprovals (uint => address);
 
   function balanceOf(address _owner) external view returns (uint256) {
       return ownerZombieCount[_owner];
