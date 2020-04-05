9d34bdffcb3867d3d6949e7816a5b51b122fb9c4
jordanonuma <jordanonuma@outlook.com>
2020-04-05 10:10:31 -1000

    Declared SafeMath for uint32.

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -4,6 +4,7 @@ import "./SafeMath.sol";
 
 contract ZombieFactory is Ownable {
     using SafeMath for uint256;
+    using SafeMath for uint32;
     event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
