11040b32c98ef83eb01a890c043a9d9940fbc200
jordanonuma <jordanonuma@outlook.com>
2020-04-21 07:00:09 -1000

    Revised for syntax consistency.

--- zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
+++ zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
@@ -14,14 +14,14 @@ contract ExampleGame is ZBGameMode  {
         changes.changePlayerDefense(Player.Player1, 15);
         changes.changePlayerDefense(Player.Player2, 15);
 
-        changes.changePlayerCurrentGooVials(Player1, 3);
-        changes.changePlayerCurrentGooVials(Player2, 3);
+        changes.changePlayerCurrentGooVials(Player.Player1, 3);
+        changes.changePlayerCurrentGooVials(Player.Player2, 3);
 
-        changes.changePlayerCurrentGoo(Player1, 3);
-        changes.changePlayerCurrentGoo(Player2, 3);
+        changes.changePlayerCurrentGoo(Player.Player1, 3);
+        changes.changePlayerCurrentGoo(Player.Player2, 3);
 
-        changes.changePlayerMaxGooVials(Player1, 8);
-        changes.changePlayerMaxGooVials(Player2, 8);
+        changes.changePlayerMaxGooVials(Player.Player1, 8);
+        changes.changePlayerMaxGooVials(Player.Player2, 8);
     } // end function beforeMatchStart()
 
 } //end contract ExampleGame{}
