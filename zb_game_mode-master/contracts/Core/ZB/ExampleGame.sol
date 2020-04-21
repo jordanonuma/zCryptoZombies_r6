b8d9e39cba3042489a1c74a7d15cd6802a25cffe
jordanonuma <jordanonuma@outlook.com>
2020-04-21 06:57:15 -1000

    Set number vials filled with goo for both players to 3.

--- zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
+++ zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
@@ -16,6 +16,9 @@ contract ExampleGame is ZBGameMode  {
 
         changes.changePlayerCurrentGooVials(Player1, 3);
         changes.changePlayerCurrentGooVials(Player2, 3);
+
+        changes.changePlayerCurrentGoo(Player1, 3);
+        changes.changePlayerCurrentGoo(Player2, 3);
     } // end function beforeMatchStart()
 
 } //end contract ExampleGame{}
