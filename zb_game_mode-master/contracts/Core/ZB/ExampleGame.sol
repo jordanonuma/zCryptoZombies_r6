b17fee43440959c16b93aa0ec082efe95c83817d
jordanonuma <jordanonuma@outlook.com>
2020-04-24 07:33:51 -1000

    Added check for card has not been banned.

--- zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
+++ zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
@@ -54,7 +54,9 @@ contract ExampleGame is ZBGameMode  {
             CardInstance[] memory newCards = new CardInstance[](gameState.playerStates[i].cardsInDeck.length);
             uint cardCount = 0;
             for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
-                
+                if(isLegalCard(gameState.playerStates[i].cardsInDeck[j])) {
+                    
+                } /end if()
             } //end for(j)
         } //end for(i)
     } // end function beforeMatchStart()
