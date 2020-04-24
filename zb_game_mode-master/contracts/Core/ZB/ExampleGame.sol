2a47b323444f32ef4e2c7bb925434c7f3c262cac
jordanonuma <jordanonuma@outlook.com>
2020-04-24 07:35:07 -1000

    Added legal card to the array newCards[] in memory.

--- zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
+++ zb_game_mode-master/contracts/Core/ZB/ExampleGame.sol
@@ -55,7 +55,7 @@ contract ExampleGame is ZBGameMode  {
             uint cardCount = 0;
             for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
                 if(isLegalCard(gameState.playerStates[i].cardsInDeck[j])) {
-                    
+                    newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
                 } /end if()
             } //end for(j)
         } //end for(i)
