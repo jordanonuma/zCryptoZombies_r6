9f0f09e49fb86c0c34a0d1778fbc6f3dd3cabd84
jordanonuma <jordanonuma@outlook.com>
2020-05-01 16:52:19 -1000

    Added test for contract CryptoZombies{}. The contract is being tested if it can create ERC721s.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -1,1 +1,6 @@
 const CryptoZombies = artifacts.require("CryptoZombies");
+
+contract("CryptoZombies", (accounts) => {
+    it("should be able to create a new zombie", () => {
+    }) //end it()
+  }) //end contract()
