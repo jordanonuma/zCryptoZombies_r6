b4ff3b99856c422422928d40955f048a66cf5b6d
jordanonuma <jordanonuma@outlook.com>
2020-05-05 07:28:22 -1000

    Removed redundant declaration.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -9,7 +9,6 @@ contract("CryptoZombies", (accounts) => {
     }); //end beforeEach()
     
     it("should be able to create a new zombie", async () => {
-        const contractInstance = await CryptoZombies.new();
         const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
         assert.equal(result.receipt.status, true);
         assert.equal(result.logs[0].args.name, zombieNames[0]);
