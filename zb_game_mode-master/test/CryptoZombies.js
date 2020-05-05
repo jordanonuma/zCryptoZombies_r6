ab0c8170e07956d4e637ee0a5ad9e2553c706d25
jordanonuma <jordanonuma@outlook.com>
2020-05-05 07:04:35 -1000

    Added frame of hook that will go at the start of each test.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -3,6 +3,9 @@ const CryptoZombies = artifacts.require("CryptoZombies");
 contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
     const contractInstance;
+    beforeEach(async () => {
+
+    }); //end beforeEach()
     
     it("should be able to create a new zombie", async () => {
         const contractInstance = await CryptoZombies.new();
