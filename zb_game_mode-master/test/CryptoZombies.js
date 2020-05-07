e7b2fbf1261e14893244ad02c0dd9db57a76633e
jordanonuma <jordanonuma@outlook.com>
2020-05-06 17:55:25 -1000

    Revised syntax.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -17,7 +17,7 @@ contract("CryptoZombies", (accounts) => {
     }) //end it()
 
     it("should not allow two zombies", async () => {
-        const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+        await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
         await utils.shouldThrow(CryptoZombies.createRandomZombie());
     }) //end it()
   }) //end contract()
