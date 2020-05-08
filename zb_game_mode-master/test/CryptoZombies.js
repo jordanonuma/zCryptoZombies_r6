a01f0d887f934bd6a0933515688fdf50fefb8211
jordanonuma <jordanonuma@outlook.com>
2020-05-07 21:05:44 -1000

    Provided frame for two context() tests for the transfer() and transferFrom() functions.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -20,4 +20,18 @@ contract("CryptoZombies", (accounts) => {
         await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
         await utils.shouldThrow(contractInstance.createRandomZombie(zombieNames[1], {from: alice}));
     }) //end it()
-  }) //end contract()
+
+    context("with the single-step transfer scenario", async () => {
+        it("should transfer a zombie", async () => {
+          // TODO: Test the single-step transfer scenario.
+        }) //end it()
+    }) //end context()
+    
+    context("with the two-step transfer scenario", async () => {
+        it("should approve and then transfer a zombie when the approved address calls transferForm", async () => {
+          // TODO: Test the two-step scenario.  The approved address calls transferFrom
+        }) //end it()
+        it("should approve and then transfer a zombie when the owner calls transferForm", async () => {
+            // TODO: Test the two-step scenario.  The owner calls transferFrom
+         }) //end it()
+    }) //end context()
