b4355e01a167060dc2f1de2ade47b3d2a4b5150b
jordanonuma <jordanonuma@outlook.com>
2020-05-09 09:47:17 -1000

    Added first part of second test where Alice approves the transfer to Bob.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -34,7 +34,9 @@ contract("CryptoZombies", (accounts) => {
     
     xcontext("with the two-step transfer scenario", async () => {
         it("should approve and then transfer a zombie when the approved address calls transferForm", async () => {
-          // TODO: Test the two-step scenario.  The approved address calls transferFrom
+          // Test the two-step scenario.  The approved address calls transferFrom
+            const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+            const zombieId = result.logs[0].args.zombieId.toNumber();
         }) //end it()
         it("should approve and then transfer a zombie when the owner calls transferForm", async () => {
             // TODO: Test the two-step scenario.  The owner calls transferFrom
