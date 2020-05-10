f089ebd1ef8c900b68d09ef0d6b94e41b37f1b98
jordanonuma <jordanonuma@outlook.com>
2020-05-10 09:16:51 -1000

    Added colon for syntax.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -44,7 +44,7 @@ contract("CryptoZombies", (accounts) => {
             // Test the two-step scenario.  The owner calls transferFrom
             const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
             const zombieId = result.logs[0].args.zombieId.toNumber();
-            await contractInstance.approve(bob, zombieId, {from alice});
+            await contractInstance.approve(bob, zombieId, {from: alice});
             await contractInstance.transferFrom(alice, bob, zombieId, {from: alice});
             const newOwner = await contractInstance.ownerOf(zombieId);
             assert.equal(newOwner, bob);
