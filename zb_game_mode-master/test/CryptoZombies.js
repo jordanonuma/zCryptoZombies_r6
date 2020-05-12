abe9381081d9889b2ddc27ef8d96d8c7a55668c3
jordanonuma <jordanonuma@outlook.com>
2020-05-11 17:39:41 -1000

    Added comment for both types of zombie transfers.

--- zb_game_mode-master/test/CryptoZombies.js
+++ zb_game_mode-master/test/CryptoZombies.js
@@ -39,7 +39,7 @@ contract("CryptoZombies", (accounts) => {
             const zombieId = result.logs[0].args.zombieId.toNumber();
             await contractInstance.approve(bob, zombieId, {from: alice});
             await contractInstance.transferFrom(alice, bob, zombieId, {from: bob});
-        }) //end it()
+        }) //end it() that tests Alice approving and Bob pulling transfer
         it("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
             // Test the two-step scenario.  The owner calls transferFrom
             const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
@@ -48,7 +48,7 @@ contract("CryptoZombies", (accounts) => {
             await contractInstance.transferFrom(alice, bob, zombieId, {from: alice});
             const newOwner = await contractInstance.ownerOf(zombieId);
             assert.equal(newOwner, bob);
-         }) //end it()
+         }) //end it() that tests Alic approving and transfering
          it("zombies should be able to attack another zombie", async () => {
             let result;
             result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
