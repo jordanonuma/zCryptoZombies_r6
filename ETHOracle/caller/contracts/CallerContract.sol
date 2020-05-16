2a61b2084b4349cfb9f6400c6daa2ee9ed8ab66d
jordanonuma <jordanonuma@outlook.com>
2020-05-15 22:44:45 -1000

    Renamed function to include instance.

--- ETHOracle/caller/contracts/CallerContract.sol
+++ ETHOracle/caller/contracts/CallerContract.sol
@@ -2,7 +2,8 @@ pragma solidity 0.5.0;
 
 contract CallerContract {
     address private oracleAddress;
-    function setOracleAddress(address _oracleInstanceAddress) public {
+
+    function setOraclInstanceAddress(address _oracleInstanceAddress) public {
         oracleAddress = _oracleInstanceAddress;
-    } //end function setOracleAddress()
+    } //end function setOracleInstanceAddress()
 } //end contract CallerContract{}
