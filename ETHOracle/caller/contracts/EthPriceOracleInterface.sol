dd7df1d2a5c0c0fb61b6daa622ee81ffcabbe3b0
jordanonuma <jordanonuma@outlook.com>
2020-05-16 08:48:21 -1000

    Declared interface variable called 'oracleInstance'.

--- ETHOracle/caller/contracts/EthPriceOracleInterface.sol
+++ ETHOracle/caller/contracts/EthPriceOracleInterface.sol
@@ -2,5 +2,7 @@ pragma solidity 0.5.0;
 import "./EthPriceOracleInterface.sol";
 
 contract EthPriceOracleInterface {
-  function getLatestEthPrice() public returns (uint256);
+    EthPriceOracleInterface private oracleInstance;
+    
+    function getLatestEthPrice() public returns (uint256);
 } //end contract EthPriceOracleInterface{}
