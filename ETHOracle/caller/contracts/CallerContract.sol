0b6324e8b026afa136bef6ac6e12d2dd11e5f3e4
jordanonuma <jordanonuma@outlook.com>
2020-05-15 22:40:27 -1000

    Added frame of function that will set the oracle's address.

--- ETHOracle/caller/contracts/CallerContract.sol
+++ ETHOracle/caller/contracts/CallerContract.sol
@@ -2,4 +2,7 @@ pragma solidity 0.5.0;
 
 contract CallerContract {
     address private oracleAddress;
+    function oracleAddress(address _oracleInstanceAddress) public {
+        
+    } //end function oracleAddress()
 } //end contract CallerContract{}
