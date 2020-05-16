bb12848d62f656d46d2ea7bd90b8bc07f0569f1e
jordanonuma <jordanonuma@outlook.com>
2020-05-16 08:50:28 -1000

    Instanatiated 'oracleInstance' interface.

--- ETHOracle/caller/contracts/CallerContract.sol
+++ ETHOracle/caller/contracts/CallerContract.sol
@@ -5,5 +5,6 @@ contract CallerContract {
 
     function setOraclInstanceAddress(address _oracleInstanceAddress) public {
         oracleAddress = _oracleInstanceAddress;
+        oracleInstance = EthPriceOracleInterface(_oracleInstanceAddress);
     } //end function setOracleInstanceAddress()
 } //end contract CallerContract{}
