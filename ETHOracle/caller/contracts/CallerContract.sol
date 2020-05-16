7ac61486357c43d3167ef06f170698cc89d3fd72
jordanonuma <jordanonuma@outlook.com>
2020-05-15 22:43:48 -1000

    Renamed function because it'll allow user to set address.

--- ETHOracle/caller/contracts/CallerContract.sol
+++ ETHOracle/caller/contracts/CallerContract.sol
@@ -2,7 +2,7 @@ pragma solidity 0.5.0;
 
 contract CallerContract {
     address private oracleAddress;
-    function oracleAddress(address _oracleInstanceAddress) public {
-        oracleAddress = _address;
-    } //end function oracleAddress()
+    function setOracleAddress(address _oracleInstanceAddress) public {
+        oracleAddress = _oracleInstanceAddress;
+    } //end function setOracleAddress()
 } //end contract CallerContract{}
