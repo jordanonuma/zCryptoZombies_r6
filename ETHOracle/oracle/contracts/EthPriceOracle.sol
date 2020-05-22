fb22c2480fc1f35442e9df4399b80b7e48139b71
jordanonuma <jordanonuma@outlook.com>
2020-05-21 17:37:11 -1000

    Added frame of function that will call oracle for ETH's price.

--- ETHOracle/oracle/contracts/EthPriceOracle.sol
+++ ETHOracle/oracle/contracts/EthPriceOracle.sol
@@ -2,10 +2,13 @@ pragma solidity 0.5.0;
 import "../../../Ownable.sol";
 import "./CallerContractInterface.sol";
 contract EthPriceOracle is Ownable {
-  uint private randNonce = 0;
-  uint private modulus = 1000;
-  mapping(uint256=>bool) pendingRequests;
-  event GetLatestEthPriceEvent(address callerAddress, uint id);
-  event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
+    uint private randNonce = 0;
+    uint private modulus = 1000;
+    mapping(uint256=>bool) pendingRequests;
+    event GetLatestEthPriceEvent(address callerAddress, uint id);
+    event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
   
+    function getLatestEthPrice() public returns(uint256) {
+      
+    } //end function getLatestEthPrice()
 } //end contract EthPriceOracle{}
