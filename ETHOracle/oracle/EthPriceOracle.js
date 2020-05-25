ed1a8006a8060c153c86fa8b76f22565eb216c59
jordanonuma <jordanonuma@outlook.com>
2020-05-25 11:43:03 -1000

    Revised to clarify JS function is asynchronous.

--- ETHOracle/oracle/EthPriceOracle.js
+++ ETHOracle/oracle/EthPriceOracle.js
@@ -8,7 +8,7 @@ const MAX_RETRIES = process.env.MAX_RETRIES || 5
 const OracleJSON = require('./oracle/build/contracts/EthPriceOracle.json')
 var pendingRequests = []
 
-getOracleContract(web3.js) {
+async function getOracleContract(web3.js) {
     const networkId = await web3js.eth.net.getId()
     return new web3js.eth.Contract(OracleJSON.abi, OracleJSON.networks[networkId].address)
 } //end getOracleContract()
