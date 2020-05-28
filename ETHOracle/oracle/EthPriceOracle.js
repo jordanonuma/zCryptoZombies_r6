1264866b47ed23a35e8824df4c6f612979a9af06
jordanonuma <jordanonuma@outlook.com>
2020-05-27 19:29:10 -1000

    Revised to push to pendingRequests[] array.

--- ETHOracle/oracle/EthPriceOracle.js
+++ ETHOracle/oracle/EthPriceOracle.js
@@ -39,5 +39,5 @@ async function filterEvents(oracleContract, web3js) {
 async function addRequestToQueue(event) {
     const callerAddress = event.returnValues.callerAddress
     const id = event.returnValues.id
-    console.log({callerAddress, id})    
+    pendingRequests.push({callerAddress, id})    
 } //end function addRequestToQueue()
