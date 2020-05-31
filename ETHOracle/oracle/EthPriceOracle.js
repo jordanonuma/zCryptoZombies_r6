3f44c3c44919f164422ac717dd17fd4a071193f7
jordanonuma <jordanonuma@outlook.com>
2020-05-31 09:49:29 -1000

    Sets the price.

--- ETHOracle/oracle/EthPriceOracle.js
+++ ETHOracle/oracle/EthPriceOracle.js
@@ -56,6 +56,7 @@ async function processRequest (oracleContract, ownerAddress, id, callerAddress) 
     while (retries < MAX_RETRIES) {
         try {
             const ethPrice = await retrieveLatestEthPrice()
+            await setLatestEthPrice(oracleContract, callerAddress, ownerAddress, ethPrice, id)
         } catch (error) {
             
         } //end catch()
