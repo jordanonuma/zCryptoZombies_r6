d47f01a4c84d83f5768524907dbe4b073e6328cc
jordanonuma <jordanonuma@outlook.com>
2020-06-01 20:18:13 -1000

    Returns a 0 price on error.

--- ETHOracle/oracle/EthPriceOracle.js
+++ ETHOracle/oracle/EthPriceOracle.js
@@ -60,7 +60,7 @@ async function processRequest (oracleContract, ownerAddress, id, callerAddress) 
             return
         } catch (error) {
             if (retries === MAX_RETRIES - 1) {
-          
+                setLatestEthPrice(oracleContract, callerAddress, ownerAddress, 0, id)
             } //end if()
         } //end catch()
     } //end while()
