9ff04edcc58e906d2174cbd2530aca28a4e26421
jordanonuma <jordanonuma@outlook.com>
2020-06-06 08:51:01 -1000

    Re-defined constant 'privateKey' to be used with caller contract.

--- ETHOracle/caller/truffle-config.js
+++ ETHOracle/caller/truffle-config.js
@@ -107,7 +107,8 @@ module.exports = {
   networks: {
     extdev: {
       provider: function () {
-        const privateKey = fs.readFileSync(path.join(__dirname, 'oracle_private_key'), 'utf-8')
+        // change out the following with the below: const privateKey = fs.readFileSync(path.join(__dirname, 'oracle_private_key'), 'utf-8')
+        const privateKey = fs.readFileSync(path.join(__dirname, 'caller_private_key'), 'utf-8')`
         const chainId = 'extdev-plasma-us1'
         const writeUrl = 'wss://extdev-plasma-us1.dappchains.com/websocket'
         const readUrl = 'wss://extdev-plasma-us1.dappchains.com/queryws'
