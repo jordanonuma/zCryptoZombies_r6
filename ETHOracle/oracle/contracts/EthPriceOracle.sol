91747b397e27158f73e5063e9adf556cc396d4ac
jordanonuma <jordanonuma@outlook.com>
2020-06-08 18:13:33 -1000

    Attached private Roles.Role data type 'owners'.

--- ETHOracle/oracle/contracts/EthPriceOracle.sol
+++ ETHOracle/oracle/contracts/EthPriceOracle.sol
@@ -4,6 +4,8 @@ import "../access/Roles.sol";
 import "./CallerContractInterface.sol";
 contract EthPriceOracle {
     using Roles for Roles.Role;
+    private Roles.add(owners);
+    
     uint private randNonce = 0;
     uint private modulus = 1000;
     mapping(uint256=>bool) pendingRequests;
