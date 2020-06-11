0490c95132dc1035eb92cd71febdbcbef46a7808
jordanonuma <jordanonuma@outlook.com>
2020-06-10 21:02:35 -1000

    Defined new event that will notify front end when an oracle has been addded.

--- ETHOracle/oracle/contracts/EthPriceOracle.sol
+++ ETHOracle/oracle/contracts/EthPriceOracle.sol
@@ -12,6 +12,7 @@ contract EthPriceOracle {
     mapping(uint256=>bool) pendingRequests;
     event GetLatestEthPriceEvent(address callerAddress, uint id);
     event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
+    event AddOracleEvent(address oracleAddress);
   
     constructor (address _owner) public {
         owners.add(_owner);
